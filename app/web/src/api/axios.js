fc2033a0493de59b2920cd4c98c38fba554b07f7
Samit Rungruang <nareezstudio@outlook.com>
2026-04-19 19:19:21 +0700

    fix submodules on windows#13

--- app/web/src/api/axios.js
+++ app/web/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:5000/api', 
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', // ใช้ตัวแปรนี้แทน localhost:5000
+  baseURL: import.meta.env.VITE_API_URL || 'http://bussababun.com/api', // ใช้ตัวแปรนี้แทน localhost:5000
   withCredentials: true
 });
 
