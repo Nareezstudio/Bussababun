bf2ddf134e25f58c4fc98fe8b42c69533d3fbcd2
Samit Rungruang <nareezstudio@outlook.com>
2026-04-19 22:28:17 +0700

    fix submodules on windows#16

--- app/backend/src/routes/contestRoutes.js
+++ app/backend/src/routes/contestRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const contestController = require('../controllers/contestController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 
 router.get('/', contestController.getContests);
 router.post('/submit', protect, contestController.submitEntry);
